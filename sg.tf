d7dcca6105b1a9e448d43e34cd2014f5c4a141b6
Atanas Margushev <atanas.margushev@itgix.com>
2024-09-10 17:20:55 +0300

    wireguard

--- sg.tf
+++ sg.tf
@@ -4,13 +4,6 @@ resource "aws_security_group" "wireguard_sg" {
   vpc_id = var.vpc_id
   name   = "${var.project}-${var.env}-wireguard-sg"
 
-  ingress {
-    protocol  = "-1"
-    self      = true
-    from_port = 0
-    to_port   = 0
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -18,8 +11,18 @@ resource "aws_security_group" "wireguard_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = {
-    Name      = "${var.project}-${var.env}-wireguard-sg"
-    ManagedBy = "Terraform"
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    self        = false
+  }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = []
+    self        = true
   }
 }
